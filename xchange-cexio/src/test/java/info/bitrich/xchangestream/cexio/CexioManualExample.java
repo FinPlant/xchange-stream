3dfb111e7fc1b3d3ec2d90431d36440d1710373d
Vicq <victor.didenko@brio.lv>
2018-03-26 11:44:34 +0300

    CEXIO handling order message

--- xchange-cexio/src/test/java/info/bitrich/xchangestream/cexio/CexioManualExample.java
+++ xchange-cexio/src/test/java/info/bitrich/xchangestream/cexio/CexioManualExample.java
@@ -1,6 +1,5 @@
 package info.bitrich.xchangestream.cexio;
 
-import info.bitrich.xchangestream.core.StreamingExchange;
 import info.bitrich.xchangestream.core.StreamingExchangeFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -12,10 +11,10 @@ public class CexioManualExample {
     private static final Logger LOG = LoggerFactory.getLogger(CexioManualExample.class);
 
     public static void main(String[] args) throws IOException {
-        StreamingExchange exchange = StreamingExchangeFactory.INSTANCE.createExchange(
+        CexioStreamingExchange exchange = (CexioStreamingExchange) StreamingExchangeFactory.INSTANCE.createExchange(
                 CexioStreamingExchange.class.getName());
         CexioProperties properties = new CexioProperties();
-        ((CexioStreamingExchange) exchange).setCredentials(properties.getApiKey(), properties.getSecretKey());
+        exchange.setCredentials(properties.getApiKey(), properties.getSecretKey());
         exchange.connect().blockingAwait();
 
         try {
